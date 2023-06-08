8526c2588376b83346ee38d4471a7b3e242fc91e
lamtev178 <lamtev2000@gmail.com>
2023-06-08 17:02:15 +0300

    initial commit

---
+++ cli.ts
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+import { execSync } from 'node:child_process';
+import { copyFile } from 'node:fs/promises';
+import { writeFileSync } from 'fs';
+import { mkdirp } from 'mkdirp';
+import nodePath from 'path';
+import minimist from 'minimist';
+// import 'dotenv/config'
+// console.log(process.env);
+// for(let i of process.argv){
+//     if(process.env[`${i}`]){
+//         console.log(i + " :", process.env[`${i}`]);
+
+//     }
+// }
+const args = process.argv.slice(2);
+
+type Argv = {
+  out: string;
+  export?: string;
+  help: boolean;
+  'package-version': string;
+};
+const argv = minimist<Argv>(args, {
+  string: ['out', 'export', 'package-version'],
+  alias: {
+    out: ['o'],
+    export: ['e'],
+  },
+});
+
+const packageVersion = argv['package-version'];
+const outputPath = argv.out;
+const inputPath = argv._[0];
+
+if (!packageVersion) {
+  console.error('You have to specify package-version');
+  process.exit(1);
+}
+
+args.splice(
+  args.findIndex((el) => el === '--package-version'),
+  2,
+);
+
+execSync(`taxios-generate ${args.join(' ')}`);
+
+async function main() {
+  try {
+    await mkdirp(nodePath.dirname(`typesPackage/${outputPath}`));
+
+    await copyFile(outputPath, `typesPackage/${outputPath}`);
+
+    writeFileSync('./typesPackage/package.json', JSON.stringify(config), 'utf8');
+    execSync(`cd typesPackage && npm publish`);
+  } catch (error) {
+    console.log('An error has occurred ', error);
+  }
+}
+
+main();
+
+const config = {
+  name: `taxios_${inputPath.split('//')[1].split('/')[0]}`,
+  version: packageVersion,
+};
---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+const makeConfig = require('@web-bee-ru/commitlint-config');
+
+module.exports = makeConfig({
+  commitTypes: [
+    'feat', // @NOTE: implementation of functionality
+    'fix', // @NOTE: bugfix
+    'wip', // @NOTE: work in progress
+    'WIP', // @NOTE: work in progress
+    'ci', // @NOTE: continuous integration related issues
+    'chore', // @NOTE: should be moved in the starter-project
+    'build', // @NOTE: build settings
+    'refactor', // @NOTE: refactor
+    'docs', // @NOTE: docs
+    'perf', // @NOTE: performance issues
+    'style', // @NOTE: style fixes
+    'deps', // @NOTE: dependencies update
+    'test', // @NOTE: tests
+  ],
+});
